99b90cdc40a607bbfc9c19cf09cb7ed45d5444ab
Josue Maldonado <elrepoio1234@gmail.com>
2024-05-08 13:31:33 -0600

    ejemplo de una funcion,integral y derivada

--- Tics.m
+++ Tics.m
@@ -3,3 +3,15 @@
 %%TI202 CALCULO INTEGRAL
 pkg load symbolic
 syms x
+
+%%definir una funcion
+fx=x
+%%derivar
+disp('derivada')
+diff(fx,x)
+%%integrar indefinida
+disp('integral indefinida')
+ifx=int(fx,x)
+%%integrar definida
+disp('integral definida')
+ifx=double(int(fx,x,0,1))
